5eae672fcd2349c27e9d0c39f87ea89354e127da
Rhian McClelland <56872919+RhianMcClelland@users.noreply.github.com>
2023-05-04 04:13:03 +0000

    Add missed verison

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 import setuptools 
 
 setuptools.setup(name='silverwinner',
-      version='0.0.2', 
+      version='0.0.3', 
       author = 'Rhian McClelland',
       python_requires='>=3.11.0',
       package_dir={"": "src"},
