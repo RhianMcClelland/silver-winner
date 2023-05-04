07ebe22819868d35b9cbdac29581c2cce077d53c
Rhian McClelland <56872919+RhianMcClelland@users.noreply.github.com>
2023-05-04 03:47:07 +0000

    configure base template

---
+++ setup.py
@@ -0,0 +1,10 @@
+
+import setuptools 
+
+setuptools.setup(name='silverwinner',
+      version='0.0.1', 
+      author = 'Rhian McClelland',
+      python_requires='>=3.11.0',
+      package_dir={"": "src"},
+      packages=setuptools.find_packages(where="src")
+      ) 
